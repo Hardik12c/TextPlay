55c878506290b5a0474fd52cc6cb4810fd639fd1
Hardik <hardik12c@gmail.com>
2022-08-01 13:46:53 +0530

    in this commit i have remove some icons which are displayed by react initially so that the app must be clear

--- src/components/form.js
+++ src/components/form.js
@@ -10,6 +10,18 @@ export default function Form(props) {
     const handlelowClick=()=>{
         SetText(text.toLowerCase());
     }
+    const handcleartext=()=>{
+        SetText('');
+    } 
+    const downloadTxtFile = () => {
+        const element = document.createElement("a");
+        const file = new Blob([text], {
+          type: "text/plain"
+        });
+        element.href = URL.createObjectURL(file);
+        element.download = "myFile.txt";
+        element.click();
+    }
     const [text, SetText] = useState("");
 
     return (
@@ -19,12 +31,14 @@ export default function Form(props) {
                 <textarea  id="areafortext" value={text} onChange={handleonchange} placeholder="Enter text here" cols="150" rows="10"></textarea>
                 <button className='btn' onClick={handleUpClick}>Convert to UpperCase</button>
                 <button className='btn' onClick={handlelowClick}>Convert to Lowercase</button>
+                <button className='btn' onClick={handcleartext}>Clear Text</button>
+                <button className='btn' onClick={downloadTxtFile}>Download Text</button>
             </div>
             <div className="summary">
                 <h1>{props.title2}</h1>
                 <p>{(text.split(' ').filter(word => word !== '')).length} words and {text.length} characters</p>
                 <h4>Average time to read</h4>
-                <span>{0.008*(text.split('').length)} minutes</span>
+                <span>{0.008*((text.split(' ').filter(word => word !== '')).length)} minutes</span>
             </div>
         </div>
     )
